a19c84638ddd2bb239ae8c4948cad9013aef7a0f
leonelwill <leonel.will@gmail.com>
2026-01-16 14:40:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def gerar_texto(prompt_sistema, prompt_usuario):
         
         chat_completion = client.chat.completions.create(
             # Modelo Mixtral (Excelente, rápido e criativo)
-            model="model="llama3-70b-8192", 
+            model="llama3-70b-8192", 
             messages=[
                 {"role": "system", "content": prompt_sistema},
                 {"role": "user", "content": prompt_usuario},
